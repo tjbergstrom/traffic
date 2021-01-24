bbaae89cd2f0589bb3760422e08b59682e3a3cee
T <tjbergstrom@alaska.edu>
2021-01-23 16:10:37 -0900

    added detect only multi-proc

--- traffic.py
+++ traffic.py
@@ -22,10 +22,13 @@ if __name__ == '__main__':
 	ap.add_argument("-o", "--output", required=False)
 	ap.add_argument("-v", "--playvid", type=bool, default=False)
 	ap.add_argument("-w", "--width", type=int, default=None)
+	ap.add_argument("-d", "--detect_only", type=bool, default=False)
+	ap.add_argument("-t", "--detect_and_track", type=bool, default=False)
 	args = vars(ap.parse_args())
 
 	in_vid = args["input"]
 	out_vid = args["output"]
+	width = args["width"]
 
 	if not os.path.isfile(in_vid):
 		sys.exit(f"\'{in_vid}\' is not a filepath")
@@ -35,14 +38,24 @@ if __name__ == '__main__':
 		sys.exit(f"No output file specified \'{out_vid}\'")
 	if not out_vid and not args["playvid"]:
 		sys.exit(f"Not saving or displaying output?")
+	if width and (width < 360 or width > 1800):
+		sys.exit(f"Width \'{width}\' out of range")
 	if out_vid and os.path.isfile(out_vid):
 		print(f"Warning: will be over-writing output video \'{out_vid}\'")
 		time.sleep(3.0)
 
 	start = time.time()
-	Traffic_Detection(args["width"]).read_video(in_vid, out_vid, args["playvid"])
-
+	if args["detect_and_track"]:
+		Traffic_Detection(width).read_video(in_vid, out_vid, args["playvid"])
+	elif args["detect_only"]:
+		if width:
+			os.system(f"python3 detect_only.py -i {in_vid} -o {out_vid} -w {width}")
+		else:
+			os.system(f"python3 detect_only.py -i {in_vid} -o {out_vid}")
+	else:
+		sys.exit(f"Need an arg -d or -t")
 	print(f"Finished processing video ({time.time()-start:.2f} seconds)")
+
 	if out_vid:
 		if os.path.isfile(out_vid):
 			print("Output video successfully saved")
