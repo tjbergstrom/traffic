9765f3b991beafcb3d64e2048fd2e8fc3067eb16
T <tjbergstrom@alaska.edu>
2021-02-03 10:24:14 -0900

    add helptxt

--- traffic.py
+++ traffic.py
@@ -21,17 +21,19 @@ import os
 
 if __name__ == '__main__':
 	ap = argparse.ArgumentParser()
-	ap.add_argument("-i", "--input", required=True)
-	ap.add_argument("-o", "--output", required=False)
-	ap.add_argument("-p", "--playvid", type=bool, default=False)
-	ap.add_argument("-w", "--width", type=int, default=None)
-	ap.add_argument("-d", "--detect_only", type=bool, default=False)
-	ap.add_argument("-t", "--detect_and_track", type=bool, default=False)
+	ap.add_argument("-i", "--input", required=True, help="input video filepath")
+	ap.add_argument("-o", "--output", required=False, help="save output video filepath")
+	ap.add_argument("-p", "--playvid", type=bool, default=False, help="play video while processing")
+	ap.add_argument("-w", "--width", type=int, default=None, help="resize frame width")
+	ap.add_argument("-f", "--freq", type=int, default=5, help="detection frequency, every _ frames")
+	ap.add_argument("-d", "--detect_only", type=bool, default=False, help="no tracking")
+	ap.add_argument("-t", "--detect_and_track", type=bool, default=False, help="increased performance")
 	args = vars(ap.parse_args())
 
 	in_vid = args["input"]
 	out_vid = args["output"]
 	width = args["width"]
+	freq = args["freq"]
 
 	if not os.path.isfile(in_vid):
 		sys.exit(f"\'{in_vid}\' is not a filepath")
@@ -43,13 +45,15 @@ if __name__ == '__main__':
 		sys.exit(f"Not saving or displaying output?")
 	if width and (width < 360 or width > 1800):
 		sys.exit(f"Width \'{width}\' out of range")
+	if freq < 1 or freq > 40:
+		sys.exit(f"Detection frequency \'{freq}\' not supported")
 	if out_vid and os.path.isfile(out_vid):
 		print(f"Warning: will be over-writing output video \'{out_vid}\'")
 		time.sleep(3.0)
 
 	start = time.time()
 	if args["detect_and_track"]:
-		Traffic_Detection(width).read_video(in_vid, out_vid, args["playvid"])
+		Traffic_Detection(width, freq).read_video(in_vid, out_vid, args["playvid"])
 	elif args["detect_only"]:
 		if width:
 			os.system(f"python3 detect_only.py -i {in_vid} -o {out_vid} -w {width}")
